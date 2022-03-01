d2a5309a1b626ad2dc753d8fd183e5faab8eb99e
sabygil <richards2109@gmail.com>
2022-03-01 02:19:08 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
         <p>My name is Sabier Richards, and I'm going to master React!</p>
         <a
           className='App-link'
-          href='https://reactjs.org'
+          href='https://github.com/SabyGil'
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
